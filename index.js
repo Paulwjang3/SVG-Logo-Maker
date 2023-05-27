91a7f70ef6783985d6ca147793b728d8ebb1bbd1
Paulwjang3 <128998742+Paulwjang3@users.noreply.github.com>
2023-05-27 06:55:44 -0400

    fixed syntax errors

--- index.js
+++ index.js
@@ -10,26 +10,26 @@ function writeToFile(fileName, response) {
 
     svgText += "<g>";
 
-    svgText += `${answers.shape}`;
+    svgText += `${response.shape}`;
 
     let shapeChoice;
 
-    if (answers.shape === "Triangle") {
+    if (response.shape === "Triangle") {
         shapeChoice = new Triangle();
-        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
-      } else if (answers.shape === "Square") {
+        svgText += `<polygon points="150, 18 244, 182 56, 182" fill="${response.shapeBackgroundColor}"/>`;
+      } else if (response.shape === "Square") {
         shapeChoice = new Square();
-        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+        svgText += `<rect x="73" y="40" width="160" height="160" fill="${response.shapeBackgroundColor}"/>`;
       } else {
         shapeChoice = new Circle();
-        svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+        svgText += `<circle cx="150" cy="115" r="80" fill="${response.shapeBackgroundColor}"/>`;
       }
 
-      svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
-      svgString += "</g>";
-      svgString += "</svg>";
+      svgText += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${response.textColor}">${response.text}</text>`;
+      svgText += "</g>";
+      svgText += "</svg>";
 
-      fs.writeFile(fileName, svgString, (err) => {
+      fs.writeFile(fileName, svgText, (err) => {
         err ? console.log(err) : console.log("Generated logo.svg");
       });
 }
